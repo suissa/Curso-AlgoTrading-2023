71c25e85025ca21915643381237ab6bb82f6e77a
Suissinha <jnascimento@gmail.com>
2023-07-08 16:27:14 -0300

    Arrumar os inputs para cada indicator

--- projeto-final/get.indicators.js
+++ projeto-final/get.indicators.js
@@ -77,11 +77,7 @@ let input = {
   low: [], // preços baixos
   close: [], // preços de fechamento
   volume: [], // volumes de negociação
-  period: 14, // período para os indicadores
-
-  // psar
-  step: 0.02,
-  max: 0.2,
+  period: 1, // período para os indicadores
 };
 
 async function fetchCurrentPrice() {
@@ -114,62 +110,69 @@ async function fetchData() {
 
     const data = response.data;
     const latestData = data[data.length - 1];
-
+    console.log(`Latest data: ${latestData}`);
     const open = parseFloat(latestData[1])
     const high = parseFloat(latestData[2])
     const low = parseFloat(latestData[3])
     const close = parseFloat(latestData[4])
     const volume = parseFloat(latestData[5])
-    input.open.push();
-    input.high.push();
-    input.low.push();
-    input.close.push();
-    input.volume.push();
+    input.open.push(open);
+    input.high.push(high);
+    input.low.push(low);
+    input.close.push(close);
+    input.volume.push(volume);
 
     // Check if we have enough data to start calculating indicators
     if(input.close.length > input.period) {
-      const sma = new ti.SMA(input).getResult();
-      const ema = new ti.EMA(input).getResult();
-      const wma = new ti.WMA(input).getResult();
-      const rsi = new ti.RSI(input).getResult();
-      const bollingerBands = new ti.BollingerBands(input).getResult();
-      const adx = new ti.ADX(input).getResult();
-      const atr = new ti.ATR(input).getResult();
-      const cci = new ti.CCI(input).getResult();
-      const forceIndex = new ti.ForceIndex(input).getResult();
-      const macd = new ti.MACD(input).getResult();
-      const obv = new ti.OBV.calculate(input);
-      const t3 = new ti.T3(input).getResult();
-      const wema = new ti.WEMA(input).getResult();
-      const roc = new ti.ROC(input).getResult();
-      const stoch = new ti.Stochastic.calculate(input);
-      const williamsR = new ti.WilliamsR.calculate(input);
-      const ad = new ti.AD.calculate(input);
-      const awesomeOscillator = new ti.AwesomeOscillator.calculate(input);
-      const kst = new ti.KST.calculate(input);
-      const psar = new ti.PSAR.calculate({...input, step: 0.02, max: 0.2});
-      const truerange = new ti.TrueRange.calculate(input);
-      const mfi = new ti.MFI.calculate(input);
-      const averageGain = new ti.AverageGain.calculate(input);
-      const averageLoss = new ti.AverageLoss.calculate(input);
-      const sd = new ti.SD.calculate(input);
-      const pdi = new ti.PDI.calculate(input);
-      const mdi = new ti.MDI.calculate(input);
-      const dx = new ti.DX.calculate(input);
-      const ao = new ti.AbsoluteOscillator.calculate(input);
-      const trix = new ti.TRIX.calculate(input);
-      const vwma = new ti.VWMA.calculate(input);
-      const vwap = new ti.VWAP.calculate(input);
-      const smma = new ti.SMMA.calculate(input);
-      const vosc = new ti.VOSC.calculate(input);
-      const apo = new ti.APO.calculate(input);
-      const linregslope = new ti.LinRegSlope.calculate(input);
-      const linregintercept = new ti.LinRegIntercept.calculate(input);
-      const linreg = new ti.LinReg.calculate(input);
-      const stddev = new ti.StdDev.calculate(input);
-      const variance = new ti.Variance.calculate(input);
-      const truestrength = new ti.TrueStrength.calculate(input);
+      const sma = new ti.SMA(input.close).getResult();
+      // const ema = new ti.EMA(input.close).getResult();
+      // const wma = new ti.WMA(input.close).getResult();
+      // const rsi = new ti.RSI(input.close).getResult();
+      const bollingerBands = new ti.BollingerBands({...input, period: 20,
+        stdDev: 2,
+        values: data.slice(-20).map(d => parseFloat(d[4]))
+      }).getResult();
+      // const adx = new ti.ADX(input).getResult();
+      // const atr = new ti.ATR(input).getResult();
+      // const cci = new ti.CCI(input).getResult();
+      // const forceIndex = new ti.ForceIndex(input).getResult();
+      // const macd = new ti.MACD(input).getResult();
+      // const obv = new ti.OBV.calculate(input);
+      // const t3 = new ti.T3(input).getResult();
+      // const wema = new ti.WEMA(input).getResult();
+      // const roc = new ti.ROC(input).getResult();
+      // const stoch = new ti.Stochastic.calculate(input);
+      // const williamsR = new ti.WilliamsR.calculate(input);
+      // const ad = new ti.AD.calculate(input);
+      // const awesomeOscillator = new ti.AwesomeOscillator.calculate(input);
+      // const kst = new ti.KST.calculate(input);
+      // const psar = new ti.PSAR.calculate({...input, step: 0.02, max: 0.2});
+      // const truerange = new ti.TrueRange.calculate(input);
+      // const mfi = new ti.MFI.calculate(input);
+      // const averageGain = new ti.AverageGain.calculate(input);
+      // const averageLoss = new ti.AverageLoss.calculate(input);
+      // const sd = new ti.SD.calculate(input);
+      // const pdi = new ti.PDI.calculate(input);
+      // const mdi = new ti.MDI.calculate(input);
+      // const dx = new ti.DX.calculate(input);
+      // const ao = new ti.AbsoluteOscillator.calculate(input);
+      // const trix = new ti.TRIX.calculate(input);
+      // const vwma = new ti.VWMA.calculate(input);
+      // const vwap = new ti.VWAP.calculate(input);
+      // const smma = new ti.SMMA.calculate(input);
+      // const vosc = new ti.VOSC.calculate(input);
+      // const apo = new ti.APO.calculate(input);
+      // const linregslope = new ti.LinRegSlope.calculate(input);
+      // const linregintercept = new ti.LinRegIntercept.calculate(input);
+      // const linreg = new ti.LinReg.calculate(input);
+      // const stddev = new ti.StdDev.calculate(input);
+      // const variance = new ti.Variance.calculate(input);
+      // const truestrength = new ti.TrueStrength.calculate(input);
       
+      console.log(`SMA: ${sma}`);
+      // console.log(`EMA: ${ema}`);
+      console.log(`bollingerBands`, bollingerBands);
+      return false;
       PriceData.create({
         timestamp: Date.now(),
         symbol: "BTCUSDT",
@@ -184,11 +187,7 @@ async function fetchData() {
           ema,
           wma,
           rsi,
-          bollingerBands: {
-            upper: Number,
-            middle: Number,
-            lower: Number
-          },
+          bollingerBands: bollingerBands[0],
           adx,
           atr,
           cci,
@@ -241,4 +240,4 @@ async function fetchData() {
 }
 
 // Fetch data every 10 seconds
-setInterval(fetchData, 10 * 1000);
+setInterval(fetchData, 1 * 1000);
