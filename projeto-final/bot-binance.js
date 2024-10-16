a6645f8f4a2b262dc31f5fc40b24cc8ba9102a0a
Jean Carlo Nascimento <jnascimento@gmail.com>
2024-10-16 13:49:38 -0300

    mudança no getCandles

--- projeto-final/bot-binance.js
+++ projeto-final/bot-binance.js
@@ -65,7 +65,7 @@ const getCurrentPrice = async (symbol) => {
 
 const getCandles = async (symbol = "BTCUSDC", interval = "5m") => {
   try {
-    const candles = await client.getCandles();
+    const candles = await client.getCandles("BTCUSDC");
     // console.log("Last candle: ", candles[candles.length - 1]);
     return candles;
   } catch (error) {
@@ -631,6 +631,8 @@ setInterval( async () => {
     const quantity = Math.abs(amount);
     console.log({hasOpenPosition});
     console.log({position});
+    console.log("symbol aqui ",{symbol});
+    
     
     const candles = await getCandles(symbol);
     console.log({candles});
